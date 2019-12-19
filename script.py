5eb0008c959f1042b69822d54df0603f13f19897
Equuleus-00 <daminov1607@gmail.com>
2019-12-19 23:02:59 +0800

    Initial commit

---
+++ script.py
@@ -0,0 +1,8 @@
+#Let's go first big project...
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+  destination_index = destination.index()
+  return destination_index
+get_destination_index("Los Angeles, USA")
+print(get_destination_index)
